6b80e9766f0f263c07bdffbdb2fdca940bf02c57
muheez <muheezopebiyi@gmail.com>
2026-07-14 11:58:15 -0400

    working on the first page

---
+++ skinstric-app/src/pages/homePage/Landing.tsx
@@ -0,0 +1,19 @@
+import './Landing.css'
+
+export default function Landing(){
+
+  return(
+    <>
+    <header className='header'> 
+      <p>SKINSTRIC [ INTRO ]</p>
+      </header>
+      
+
+      <main className='landing'>
+      <h1 className="main__header">Sophisticated<br />skincare</h1>
+      </main>
+     
+    </>
+   
+  ) 
+}
